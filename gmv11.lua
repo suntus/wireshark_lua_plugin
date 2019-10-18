47f222e44fa443514ab72b47b4b0d8249dcace62
suntus <suntus.4@gmail.com>
2019-10-18 17:28:26 +0800

    调整结构; 支持部分ClientHello字段

--- gmv11.lua
+++ gmv11.lua
@@ -9,15 +9,9 @@ do
         [23] = 'ApplicationData',
         [80] = 'Site2site',
     }
-    p_gm.fields.type = ProtoField.uint8('tls.record.type', 'Content Type', base.DEC, r_content_type)
-    p_gm.fields.version = ProtoField.uint16('tls.record.version', 'Version', base.HEX, {[0x0101] = 'GM v1.1'})
-    p_gm.fields.len = ProtoField.uint16("tls.record.length", "Length", base.DEC)
-
-    local p_ccs   = Proto("changecipherspec", "ChangeCipherSpec")
-    local p_alert = Proto("alert","Alert")
-    local p_hs    = Proto('handshake','Handshake')
-    local p_app   = Proto('applicationdata','ApplicationData')
-    local p_s2s   = Proto('site2site','Site2site')
+    p_gm.fields.r_type = ProtoField.uint8('tls.record.type', 'Content Type', base.DEC, r_content_type)
+    p_gm.fields.r_version = ProtoField.uint16('tls.record.version', 'Version', base.HEX, {[0x0101] = 'GM v1.1'})
+    p_gm.fields.r_len = ProtoField.uint16("tls.record.length", "Length", base.DEC)
 
     -- hanshake
     local hs_type = {
@@ -31,34 +25,44 @@ do
         [16] = "ClientKeyExchange",
         [20] = "Finished",
     }
-
-    p_hs.fields.type = ProtoField.uint8("tls.handshake.type", "Handshake Type", base.DEC, hs_type)
-    p_hs.fields.len  = ProtoField.uint24("tls.handshake.length", "Length", base.DEC)
-    p_hs.fields.version  = ProtoField.uint16('tls.handshake.version', 'Version', base.HEX, {[0x0101] = 'GM v1.1'})
-    p_hs.fields.random = ProtoField.bytes('tls.handshake.random', 'Random', base.NONE)
-    p_hs.fields.gmt_time = ProtoField.absolute_time('tls.handshake.gmt_time', 'GMT Unix Time', base.LOCAL)
-    p_hs.fields.real_random = ProtoField.bytes('tls.handshake.real_random', 'Random Bytes', base.NONE)
+    p_gm.fields.hs = ProtoField.bytes("tls.record.content", "Handshake", base.NONE)
+    p_gm.fields.hs_type = ProtoField.uint8("tls.handshake.type", "Handshake Type", base.DEC, hs_type)
+    p_gm.fields.hs_len  = ProtoField.uint24("tls.handshake.length", "Length", base.DEC)
+    p_gm.fields.hs_version  = ProtoField.uint16('tls.handshake.version', 'Version', base.HEX, {[0x0101] = 'GM v1.1'})
+    p_gm.fields.hs_random = ProtoField.bytes('tls.handshake.random', 'Random', base.NONE)
+    p_gm.fields.hs_gmt_time = ProtoField.absolute_time('tls.handshake.gmt_time', 'GMT Unix Time', base.LOCAL)
+    p_gm.fields.hs_random_bytes = ProtoField.bytes('tls.handshake.random_bytes', 'Random Bytes', base.NONE)
+    p_gm.fields.hs_sess_id_len = ProtoField.uint8('tls.handshake.session_id_len', 'Session ID Length', base.DEC)
+    p_gm.fields.hs_sess_id = ProtoField.bytes('tls.handshake.session_id', 'Session ID', base.NONE)
+    p_gm.fields.hs_cipher_suites_len = ProtoField.uint8('tls.handshake.cipher_suites_len', 'Cipher Suites Length', base.DEC)
+    p_gm.fields.hs_ciphers = ProtoField.bytes('tls.handshake.ciphers', 'Session ID', base.NONE)
 
     local function hs_handler_ch(tvb, pinfo, tree)
         local offset = 0
         local v_version = tvb(offset, 2)
         offset = offset + 2
 
-        tree:add(p_hs.fields.version, v_version)
+        tree:add(p_gm.fields.hs_version, v_version)
 
         local v_random = tvb(offset, 32)
-        local t = tree:add(p_hs.fields.random, v_random)
+        local t = tree:add(p_gm.fields.hs_random, v_random)
         local v_gmt_time = tvb(offset, 4)
         offset = offset + 4
-        t:add(p_hs.fields.gmt_time, v_gmt_time)
+        t:add(p_gm.fields.hs_gmt_time, v_gmt_time)
         local v_real_random = tvb(offset, 28)
         offset = offset + 28
-        t:add(p_hs.fields.real_random, v_real_random)
+        t:add(p_gm.fields.hs_random_bytes, v_real_random)
 
+        local v_sess_id_len = tvb(offset, 1)
+        offset = offset + 1
+        tree:add(p_gm.fields.hs_sess_id_len, v_sess_id_len)
 
-        -- todo: client_hello
+        local v_sess_id = tvb(offset, v_sess_id_len:uint())
+        offset = offset + v_sess_id_len:uint()
+        tree:add(p_gm.fields.hs_sess_id, v_sess_id)
+
+        local v_cihpers_len
     end
-
     local function hs_handler_sh(tvb, pinfo, tree)
 
     end
@@ -68,139 +72,53 @@ do
     }
 
 
-
-
     -- record content type
-    local PContentType = {
-        type = 0,
-        name = nil,
-        proto = nil,
-        handler = nil,
-    }
-
-    function PContentType:new(o, type, name, proto)
-        o = o or {}
-        setmetatable(o, self)
-        self.__index = self
-        self.type = type
-        self.name = name
-        self.proto = proto
-        return o
-    end
-
-    function PContentType:handle(tvb, pinfo, tree)
-        -- statements
-    end
-
-    local PContentType_CCS = PContentType:new()
-    local PContentType_ALERT = PContentType:new()
-    local PContentType_HS = PContentType:new()
-    local PContentType_APP = PContentType:new()
-    local PContentType_S2S =  PContentType:new()
-
-    function PContentType_CCS:new(o, type, name, proto)
-        o = o or PContentType:new(o)
-        setmetatable(o, self)
-        self.__index = self
-        self.type = type
-        self.name = name
-        self.proto = proto
-        return o;
-    end
-    function PContentType_CCS:handle(tvb, pinfo, tree)
+    function r_handler_ccs(tvb, pinfo, tree)
         -- statements
         -- TODO:
-        print(self.name)
         return true
     end
-
-
-    function PContentType_ALERT:new(o, type, name, proto)
-        o = o or PContentType:new(o)
-        setmetatable(o, self)
-        self.__index = self
-        self.type = type
-        self.name = name
-        self.proto = proto
-        return o;
-    end
-    function PContentType_ALERT:handle(tvb, pinfo, tree)
+    function r_handler_alert(tvb, pinfo, tree)
         -- statements
         -- TODO:
-        print(self.name)
         return true
     end
-
-
-    function PContentType_HS:new(o, type, name, proto)
-        o = o or PContentType:new(o)
-        setmetatable(o, self)
-        self.__index = self
-        self.type = type
-        self.name = name
-        self.proto = proto
-        return o;
-    end
-    function PContentType_HS:handle(tvb, pinfo, tree)
+    function r_handler_hs(tvb, pinfo, tree)
         local offset = 0
-        local t = tree:add(self.proto, tvb)
+        local t = tree:add(p_gm.fields.hs, tvb(offset, -1))
 
         local v_type = tvb(offset, 1)
         offset = offset + 1
-        t:add(p_hs.fields.type, v_type)
-        t:append_text(": "..hs_type[v_type:uint()])
+        t:add(p_gm.fields.hs_type, v_type)
+        t:set_text("Handshake: "..hs_type[v_type:uint()])
 
         local v_len = tvb(offset, 3)
         offset = offset + 3
-        t:add(p_hs.fields.len, v_len)
-
+        t:add(p_gm.fields.hs_len, v_len)
 
         local handler = hs_handler[v_type:uint()]
         if (handler ~= nil) then
             handler(tvb(offset, -1):tvb(), pinfo, t)
         end
 
-
         return true;
     end
-
-    function PContentType_APP:new(o, type, name, proto)
-        o = o or PContentType:new(o)
-        setmetatable(o, self)
-        self.__index = self
-        self.type = type
-        self.name = name
-        self.proto = proto
-        return o;
-    end
-    function PContentType_APP:handle(tvb, pinfo, tree)
+    function r_handler_app(tvb, pinfo, tree)
         -- TODO:
-        print(self.name)
         return true
     end
-
-    function PContentType_S2S:new(o, type, name, proto)
-        o = o or PContentType:new(o)
-        setmetatable(o, self)
-        self.__index = self
-        self.type = type
-        self.name = name
-        self.proto = proto
-        return o;
-    end
-    function PContentType_S2S:handle(tvb, pinfo, tree)
+    function r_handler_s2s(tvb, pinfo, tree)
         -- statements
         -- TODO:
-        print(self.name)
         return true
     end
 
     local r_content_proto = {
-        [20] = PContentType_CCS:new(nil, 20, p_ccs.name, p_ccs),
-        [21] = PContentType_ALERT:new(nil, 21, p_alert.name, p_alert),
-        [22] = PContentType_HS:new(nil, 22, p_hs.name, p_hs),
-        [23] = PContentType_APP:new(nil, 23, p_app.name, p_app),
-        [80] = PContentType_S2S:new(nil, 80, p_s2s.name, p_s2s),
+        [20] = r_handler_ccs,
+        [21] = r_handler_alert,
+        [22] = r_handler_hs,
+        [23] = r_handler_app,
+        [80] = r_handler_s2s,
     }
 
     local data_dis = Dissector.get('tls')
@@ -220,18 +138,17 @@ do
         end
 
         pinfo.cols.protocol = 'GMv1.1'
-        root:add(p_gm.fields.type,  v_r_type)
-        root:add(p_gm.fields.version, v_version)
+        root:add(p_gm.fields.r_type,  v_r_type)
+        root:add(p_gm.fields.r_version, v_version)
 
         local v_len = tvb(offset, 2)
         offset = offset + 2
-        root:add(p_gm.fields.len, v_len)
+        root:add(p_gm.fields.r_len, v_len)
 
         local r_type_value = v_r_type:le_uint()
-        local proto = r_content_proto[r_type_value]
-        if (proto ~= nil) then
-            print("nihao "..proto.name.." "..r_type_value)
-            return proto:handle(tvb(offset,-1):tvb(), pinfo, root)
+        local handler = r_content_proto[r_type_value]
+        if (handler ~= nil) then
+            return handler(tvb(offset, -1):tvb(), pinfo, root)
         end
 
         return false
